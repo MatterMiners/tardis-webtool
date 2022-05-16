a3b0a67dcb46e2d0987234d7a6b3d318a4894f15
Alexander Haas <mail@haas-alexander.com>
2022-05-16 14:40:07 +0200

    feat: proxy-setup

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,16 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    proxy: {
+      '/api/tardis': {
+        target: 'http://127.0.0.1:4321',
+        changeOrigin: true,
+        secure: false,
+        rewrite: (path) => path.replace(/^\/api\/tardis/, '')
+      },
+    }
+  },
   plugins: [vue()],
   resolve: {
     alias: {
