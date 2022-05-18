d030bbe994e3898fba8ec85ae2b32aca027b39f8
Alexander Haas <mail@haas-alexander.com>
2022-05-18 15:52:20 +0200

    proper html layout

--- src/App.vue
+++ src/App.vue
@@ -14,15 +14,25 @@ export default defineComponent({
 </script>
 
 <template>
-    <nav class="flex p-3 mb-2 bg-white border-none shadow-md items-center">
-        <img
-            src="./assets/TARDIS_logo.svg"
-            alt="Tardis Logo"
-            class="h-10 mr-3"
-        />
-        <h1 class="text-2xl font-sans text-slate-700">webtool</h1>
-    </nav>
+    <!DOCTYPE html>
+    <html lang="en">
+        <head>
+            <title>Tardis Webtool</title>
+        </head>
+        <body>
+            <nav
+                class="flex p-3 mb-2 bg-white border-none shadow-md items-center"
+            >
+                <img
+                    src="./assets/TARDIS_logo.svg"
+                    alt="Tardis Logo"
+                    class="h-10 mr-3"
+                />
+                <h1 class="text-2xl font-sans text-slate-700">webtool</h1>
+            </nav>
 
-    <!-- <DroneGrid /> -->
-    <Login />
+            <!-- <DroneGrid /> -->
+            <Login />
+        </body>
+    </html>
 </template>
