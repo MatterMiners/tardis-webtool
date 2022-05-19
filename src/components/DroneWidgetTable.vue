b304377bc91d13e4aabf64544006d948da86672f
Alexander Haas <mail@haas-alexander.com>
2022-05-19 13:35:27 +0200

    feat: restyle DroneWidgetTable

--- src/components/DroneWidgetTable.vue
+++ src/components/DroneWidgetTable.vue
@@ -11,40 +11,46 @@ export default defineComponent({
 </script>
 
 <template>
-    <table class="border-collapse table-auto">
-        <thead>
-            <tr>
-                <th
-                    class="font-bold p-2 rounded-md bg-slate-50 text-slate-700 border-r"
-                >
-                    Property
-                </th>
-                <th class="font-bold p-2 rounded-md bg-slate-50 text-slate-700">
-                    Property
-                </th>
-            </tr>
-        </thead>
-        <tbody>
-            <tr>
-                <th class="myth border-r">Remote Resource UUID</th>
-                <th class="myth">{{ droneData.remote_resource_uuid }}</th>
-            </tr>
-            <tr>
-                <th class="myth border-r">Site</th>
-                <th class="myth">{{ droneData.site_name }}</th>
-            </tr>
-            <tr>
-                <th class="myth border-r">Machine Type</th>
-                <th class="myth">{{ droneData.machine_type }}</th>
-            </tr>
-            <tr>
-                <th class="myth border-r">Created</th>
-                <th class="myth">{{ droneData.created }}</th>
-            </tr>
-            <tr>
-                <th class="myth border-r">Updated</th>
-                <th class="myth">{{ droneData.updated }}</th>
-            </tr>
-        </tbody>
-    </table>
+    <div class="border border-slate-300 rounded mt-2">
+        <table class="border-collapse table-auto">
+            <thead>
+                <tr>
+                    <th
+                        class="font-bold p-2 rounded-tl bg-slate-50 text-slate-700 border-r border-slate-300"
+                    >
+                        Property
+                    </th>
+                    <th
+                        class="font-bold p-2 rounded-tr bg-slate-50 text-slate-700"
+                    >
+                        Property
+                    </th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <th class="myth border-r">Remote Resource UUID</th>
+                    <th class="myth">{{ droneData.remote_resource_uuid }}</th>
+                </tr>
+                <tr>
+                    <th class="myth border-r">Site</th>
+                    <th class="myth">{{ droneData.site_name }}</th>
+                </tr>
+                <tr>
+                    <th class="myth border-r">Machine Type</th>
+                    <th class="myth">{{ droneData.machine_type }}</th>
+                </tr>
+                <tr>
+                    <th class="myth border-r">Created</th>
+                    <th class="myth">{{ droneData.created }}</th>
+                </tr>
+                <tr>
+                    <th class="mythbottom border-r rounded-bl">Updated</th>
+                    <th class="mythbottom rounded-br">
+                        {{ droneData.updated }}
+                    </th>
+                </tr>
+            </tbody>
+        </table>
+    </div>
 </template>
