bf0448794eca071ca4865615f20416e62e2c48e3
Alexander Haas <mail@haas-alexander.com>
2022-05-19 13:18:45 +0200

    refactor: extract DroneWidgetTable into separate component

--- src/components/DroneWidget.vue
+++ src/components/DroneWidget.vue
@@ -4,11 +4,13 @@ import type { PropType } from "vue";
 import type { DroneData } from "../main";
 import Button from "./util/Button.vue";
 import PowerOffButton from "./util/PowerOffButton.vue";
-import DroneWidgetTh from "./util/DroneWidgetTh.vue";
+import DroneWidgetTable from "./DroneWidgetTable.vue";
 
 export default defineComponent({
-    props: { droneData: Object as PropType<DroneData> },
-    components: { Button, PowerOffButton, DroneWidgetTh },
+    props: {
+        droneData: { type: Object as PropType<DroneData>, required: true },
+    },
+    components: { Button, PowerOffButton, DroneWidgetTable },
 });
 </script>
 
@@ -25,45 +27,7 @@ export default defineComponent({
             <span class="font-semibold">{{ droneData?.state }}</span>
         </h3>
 
-        <table class="border-collapse table-auto">
-            <thead>
-                <tr>
-                    <DroneWidgetTh
-                        label="Property"
-                        class="font-bold p-2 rounded-md bg-slate-50 text-slate-700 border-r"
-                    />
-                    <DroneWidgetTh
-                        label="Value"
-                        class="font-bold p-2 rounded-md bg-slate-50 text-slate-700"
-                    />
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                    <DroneWidgetTh
-                        label="Remote Resource UUID"
-                        class="border-r"
-                    />
-                    <DroneWidgetTh :label="droneData?.remote_resource_uuid" />
-                </tr>
-                <tr>
-                    <DroneWidgetTh label="Site" class="border-r" />
-                    <DroneWidgetTh :label="droneData?.site_name" />
-                </tr>
-                <tr>
-                    <DroneWidgetTh label="Machine Type" class="border-r" />
-                    <DroneWidgetTh :label="droneData?.machine_type" />
-                </tr>
-                <tr>
-                    <DroneWidgetTh label="Created" class="border-r" />
-                    <DroneWidgetTh :label="droneData?.created" />
-                </tr>
-                <tr>
-                    <DroneWidgetTh label="Updated" class="border-r" />
-                    <DroneWidgetTh :label="droneData?.updated" class="" />
-                </tr>
-            </tbody>
-        </table>
+        <DroneWidgetTable :droneData="droneData" />
 
         <div class="flex mx-2 justify-between">
             <Button label="Edit" class="px-7 text-lg" />
---
+++ src/components/DroneWidgetTable.vue
@@ -0,0 +1,50 @@
+<script lang="ts">
+import type { DroneData } from "@/main";
+import { defineComponent } from "vue";
+import type { PropType } from "vue";
+
+export default defineComponent({
+    props: {
+        droneData: { type: Object as PropType<DroneData>, required: true },
+    },
+});
+</script>
+
+<template>
+    <table class="border-collapse table-auto">
+        <thead>
+            <tr>
+                <th
+                    class="font-bold p-2 rounded-md bg-slate-50 text-slate-700 border-r"
+                >
+                    Property
+                </th>
+                <th class="font-bold p-2 rounded-md bg-slate-50 text-slate-700">
+                    Property
+                </th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <th class="myth border-r">Remote Resource UUID</th>
+                <th class="myth">{{ droneData.remote_resource_uuid }}</th>
+            </tr>
+            <tr>
+                <th class="myth border-r">Site</th>
+                <th class="myth">{{ droneData.site_name }}</th>
+            </tr>
+            <tr>
+                <th class="myth border-r">Machine Type</th>
+                <th class="myth">{{ droneData.machine_type }}</th>
+            </tr>
+            <tr>
+                <th class="myth border-r">Created</th>
+                <th class="myth">{{ droneData.created }}</th>
+            </tr>
+            <tr>
+                <th class="myth border-r">Updated</th>
+                <th class="myth">{{ droneData.updated }}</th>
+            </tr>
+        </tbody>
+    </table>
+</template>
