d697dbc9dd2965b745d0e7c0f0142ceee251852c
Alexander Haas <mail@haas-alexander.com>
2022-05-20 11:09:17 +0200

    feat: ColoredSlotButton added

---
+++ src/components/util/ColoredSlotButton.vue
@@ -0,0 +1,15 @@
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+    props: {
+        btnColorClass: { type: String, required: true },
+    },
+});
+</script>
+
+<template>
+    <button class="btn" :class="btnColorClass">
+        <slot />
+    </button>
+</template>
