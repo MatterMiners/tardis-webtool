e2acbce4cb972acb1ddf46cccb2cb3a0144cecc7
Alexander Haas <mail@haas-alexander.com>
2022-05-25 14:03:03 +0200

    chore: Add type checking to droneStore

--- src/store/droneStore.ts
+++ src/store/droneStore.ts
@@ -1,7 +1,6 @@
-import { getScope } from "@/util"
+import { isDroneData, type DroneData } from "@/api/apitypes"
 import axios from "axios"
 import { reactive } from "vue"
-import type { DroneData } from "../main"
 import { authStore } from "./authStore"
 
 export const droneStore = reactive({
@@ -10,17 +9,26 @@ export const droneStore = reactive({
         if (authStore.token == "") {
             throw 'Empty access_token!'
         }
-        if (authStore.getScopes() == null || !authStore.getScopes()?.includes(getScope)) {
+        if (!authStore.canGetResources()) {
             throw 'Unauthorized scope!'
         }
+
         axios.get("/api/tardis/resources/", {
             headers: {
                 accept: 'application/json',
-                Authorization: `Bearer ${authStore.token}`, // Better error handling here
+                Authorization: `Bearer ${authStore.token}`,
             }
         })
             .then(resp => {
-                this.droneData = resp.data;
+                const isDroneDataArray = (resp.data as Array<any>).every(
+                    element => isDroneData(element)
+                )
+
+                if (!isDroneDataArray) {
+                    throw "Some DroneData in response don't have the right shape";
+                }
+
+                this.droneData = resp.data
             })
             .catch(err => console.error(err))
     }
