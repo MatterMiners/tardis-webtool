4b446d0fac5ab0c6f50b6dcb4bd92014f5540419
Alexander Haas <mail@haas-alexander.com>
2022-05-26 16:45:41 +0200

    chore: removed the parseJWT function as jwt will no longer need to be parsed

--- src/util.ts
+++ src/util.ts
@@ -1,20 +1,20 @@
-import { type AccessTokenClaim, isAccessTokenClaim } from "./api/apitypes"
+// import { type UserData, isUserData } from "./api/apitypes"
 
 // source: https://stackoverflow.com/questions/38552003/how-to-decode-jwt-token-in-javascript-without-using-a-library
-export function parseJwt(token: string): AccessTokenClaim | Error {
-    const base64Url = token.split('.')[1];
-    const base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
-    const jsonPayload = decodeURIComponent(atob(base64).split('').map(function (c) {
-        return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
-    }).join(''));
+// export function parsejwt(token: string): userdata | error {
+//     const base64url = token.split('.')[1];
+//     const base64 = base64url.replace(/-/g, '+').replace(/_/g, '/');
+//     const jsonpayload = decodeuricomponent(atob(base64).split('').map(function (c) {
+//         return '%' + ('00' + c.charcodeat(0).tostring(16)).slice(-2);
+//     }).join(''));
 
-    const claim = JSON.parse(jsonPayload);
-    if (!isAccessTokenClaim(claim)) {
-        return Error("AccessTokenClaim doesn't have the right shape");
-    }
+//     const claim = json.parse(jsonpayload);
+//     if (!isuserdata(claim)) {
+//         return error("accesstokenclaim doesn't have the right shape");
+//     }
 
-    return claim
-};
+//     return claim
+// };
 
 export const getScope = "resources:get"
 export const putScope = "resources:put"
