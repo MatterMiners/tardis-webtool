ad7fe6ceb686290ddfc6be81f17708e2d50d07b4
Alexander Haas <mail@haas-alexander.com>
2022-06-02 21:18:39 +0200

    feat: 404 shows invalid path

--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -1,6 +1,7 @@
 <template>
     <div class="justify-self-center relative top-48 text-center">
         <h1 class="text-6xl font-extrabold">404 Not Found</h1>
-        <h2 class="text-2xl font-bold">This path doesn't exist!</h2>
+        <h2 class="text-3xl italic font-semibold my-2">{{ $route.path }}</h2>
+        <h3 class="text-2xl">doesn't exist!</h3>
     </div>
 </template>
