011ea86bd41bfa5f6a293c348212b3035d040262
Alexander Haas <mail@haas-alexander.com>
2022-06-02 18:11:39 +0200

    refactoring: minor

--- src/components/TableView.vue
+++ src/components/TableView.vue
@@ -1,10 +1,3 @@
-<script lang="ts">
-import { defineComponent } from "vue";
-export default defineComponent({
-    props: {},
-});
-</script>
-
 <template>
     <div
         class="bg-white shadow-md rounded justify-self-stretch self-stretch m-5 flex-grow"
