a197cc6c1e1970ae2476be5db9b68a2dd82d6541
Alexander Haas <mail@haas-alexander.com>
2022-06-02 22:36:42 +0200

    feat: logout button + additional routing

--- src/router/router.ts
+++ src/router/router.ts
@@ -1,13 +1,26 @@
-import Login from '@/components/Login.vue'
-import { createRouter, createWebHashHistory } from 'vue-router'
-import NotFound from '@/views/NotFound.vue'
+import Login from "@/components/Login.vue";
+import { createRouter, createWebHashHistory } from "vue-router";
+import NotFound from "@/views/NotFound.vue";
+import { authStore } from "@/store/authStore";
+import DroneGrid from "@/components/DroneGrid.vue";
 
 const routes = [
-    { path: '/:anything(.*)*', component: NotFound },
-    { path: '/', component: Login }
-]
+    { path: "/:unreachable(.*)*", name: "NotFound", component: NotFound },
+    { path: "/login", name: "login", component: Login },
+    { path: "/dashboard", name: "dashboard", component: DroneGrid }, // precede this with /protected
+    // { path: "/", name: "landingpage" },
+];
 
-export default createRouter({
+const router = createRouter({
     history: createWebHashHistory(),
     routes,
-})
+});
+
+// check authentication
+router.beforeEach((to, from) => {
+    if (!authStore.loggedIn && to.name !== "login" && to.name !== "NotFound") {
+        return { name: "login" };
+    }
+});
+
+export default router;
--- src/store/authStore.ts
+++ src/store/authStore.ts
@@ -1,8 +1,8 @@
 import { reactive } from "vue";
 import axios from "axios";
-import { getScope, putScope } from "../util"
-import { isUserData, } from "../api/apitypes"
-import type { UserData } from "../api/apitypes"
+import { getScope, putScope } from "../util";
+import { isUserData } from "../api/apitypes";
+import type { UserData } from "../api/apitypes";
 export const authStore = reactive({
     error: "",
     userData: {} as UserData,
@@ -10,38 +10,50 @@ export const authStore = reactive({
 
     login(user: string, passwd: string) {
         axios
-            .post(
-                "/api/tardis/user/login",
-                {
-                    password: passwd,
-                    user_name: user
-                },
-            )
+            .post("/api/tardis/user/login", {
+                password: passwd,
+                user_name: user,
+            })
             .then((resp) => {
                 if (!isUserData(resp.data.user)) {
-                    this.error = "No user data returned by api"
-                    throw this.error
+                    this.error = "No user data returned by api";
+                    throw this.error;
                 }
-                this.userData = resp.data.user
-                this.loggedIn = true
+                this.userData = resp.data.user;
+                this.loggedIn = true;
                 console.log("Success:", resp.data.msg);
             })
             .catch((err) => {
                 console.error("Error:", err.message);
                 this.error = err.message;
+            });
+    },
+
+    logout() {
+        if (!this.loggedIn) {
+            throw "Cannot log out if not logged in!";
+        }
+        axios
+            .delete("/api/tardis/user/logout")
+            .then((resp) => {
+                this.loggedIn = false;
+                console.log("Success:", resp.data.msg);
             })
+            .catch((err) => {
+                console.error("Error:", err.message);
+                this.error = err.message;
+            });
     },
 
     getScopes(): string[] {
-        return this.userData.scopes
+        return this.userData.scopes;
     },
 
     canGetResources(): boolean {
-        return this.getScopes().includes(getScope)
+        return this.getScopes().includes(getScope);
     },
 
     canPutResources(): boolean {
-        return this.getScopes().includes(putScope)
-    }
+        return this.getScopes().includes(putScope);
+    },
 });
-
