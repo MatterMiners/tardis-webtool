9279c4348bb14a9e7941e5a67ad65d8e0b6571d4
Alexander Haas <mail@haas-alexander.com>
2022-06-03 18:44:29 +0200

    feat: result type

--- src/util.ts
+++ src/util.ts
@@ -2,3 +2,41 @@ export const getScope = "resources:get";
 export const putScope = "resources:put";
 
 export const droneReloadTime = 5000;
+
+export type Result<T, E = Error> = { ok: true; val: T } | { ok: false; err: E };
+
+export function makeError<T>(err: string): Result<T> {
+    return { ok: false, err: new Error(err) };
+}
+
+export function makeOk<T>(val: T): Result<T> {
+    return { ok: true, val };
+}
+
+export function unwrap<T>(r: Result<T>): T {
+    if (!r.ok) {
+        throw r.err;
+    }
+
+    return r.val;
+}
+
+export function expect<T>(r: Result<T>, msg: string): T {
+    if (!r.ok) {
+        throw `Error ${r.err} expected: ${msg}`;
+    }
+
+    return r.val;
+}
+
+export function handle<T>(
+    r: Result<T>,
+    success: (val: T) => void,
+    handle: (err: Error) => void
+): void {
+    if (!r.ok) {
+        return handle(r.err);
+    } else {
+        return success(r.val);
+    }
+}
