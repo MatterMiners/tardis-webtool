a290ecdfd000a03e15a47fc80b60315f40386ab1
Alexander Haas <mail@haas-alexander.com>
2022-06-07 17:27:55 +0200

    fix(routing): :adhesive_bandage: Properly show the inaccessible route in 404 View
    Noew shows the route redirected from in the case of redirection

--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -1,7 +1,13 @@
 <template>
-  <div class="relative top-48 justify-self-center text-center">
-    <h1 class="text-6xl font-extrabold">404 Not Found</h1>
-    <h2 class="my-2 text-3xl italic font-semibold">{{ $route.path }}</h2>
-    <h3 class="text-2xl">doesn't exist!</h3>
-  </div>
+    <div class="relative top-48 justify-self-center text-center">
+        <h1 class="text-6xl font-extrabold">404 Not Found</h1>
+        <h2 class="my-2 text-3xl italic font-semibold">
+            {{
+                $route.redirectedFrom?.path
+                    ? $route.redirectedFrom.path
+                    : $route.path
+            }}
+        </h2>
+        <h3 class="text-2xl">doesn't exist!</h3>
+    </div>
 </template>
