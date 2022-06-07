69e8852aaffdb5c89cb23407ae84db5a27a6328c
Alexander Haas <mail@haas-alexander.com>
2022-06-07 17:50:04 +0200

    feat(api): :adhesive_bandage: Set axios timout to 2 seconds

--- src/api/util.ts
+++ src/api/util.ts
@@ -1,6 +1,8 @@
 import axios from 'axios';
 import { makeStrError, type Result, makeError } from '@/util';
 
+// set timeout to 2 seconds in case api is not reachable
+axios.defaults.timeout = 2000;
 // unfortunately has to be any (or maybe unknown) because currently the error in catch can only have the any or unknown type
 
 export function makeFetchError<T>(error: any): Result<T> {
