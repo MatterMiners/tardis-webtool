b2feca1600d1ef76bd4f6a7c57f2e3ddbc133aa3
Alexander Haas <mail@haas-alexander.com>
2022-06-07 17:57:49 +0200

    refactor(tailwind): :wrench: Standardized two colors in tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,13 @@
+const colors = require('tailwindcss/colors');
+
 module.exports = {
     content: ['./src/**/*.{vue,ts}'],
     theme: {
         extend: {
             colors: {
                 tardis: '#000097',
+                error: colors.red['500'],
+                header: colors.sky['700'],
             },
         },
     },
