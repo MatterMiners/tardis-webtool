e03af9c8d14d213014b329a7a49801da4a889e5f
Alexander Haas <mail@haas-alexander.com>
2022-06-08 10:57:29 +0200

    feat(layout): :sparkles: New filterBar component with working data fetching

--- src/components/FilterBar.vue
+++ src/components/FilterBar.vue
@@ -1,5 +1,30 @@
+<script lang="ts">
+import { useFilters } from '@/store/filterStore';
+import { storeToRefs } from 'pinia';
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+    setup() {
+        const filterStore = useFilters();
+        const { allStates, allSites, allMachineTypes } =
+            storeToRefs(filterStore);
+        return { filterStore, allStates, allSites, allMachineTypes };
+    },
+    created() {
+        this.filterStore.fetchAll();
+    },
+});
+</script>
+
 <template>
-  <section class="flex justify-center p-2 w-full bg-white">
-    <input type="text" placeholder="Filter" class="myinput" />
-  </section>
+    <section
+        class="flex justify-center p-2 bg-white shadow-md sticky z-10 top-20 w-full"
+    >
+        <input type="text" placeholder="Filter" class="myinput" />
+        <p v-for="state in allStates" class="text-xl">{{ state }}</p>
+        <p v-for="state in allSites" class="text-red-400">{{ state }}</p>
+        <p v-for="state in allMachineTypes" class="text-blue-400">
+            {{ state }}
+        </p>
+    </section>
 </template>
