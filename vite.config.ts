90d2d5c823b006ae356f355ea54b1db4bb150d67
Alexander Haas <mail@haas-alexander.com>
2022-06-08 10:54:24 +0200

    fix: :poop: Temporary fix for some error that was thrown by axios caching

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
-import {fileURLToPath, URL} from 'url';
+import { fileURLToPath, URL } from 'url';
 
-import {defineConfig} from 'vite';
+import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
@@ -20,5 +20,8 @@ export default defineConfig({
         alias: {
             '@': fileURLToPath(new URL('./src', import.meta.url)),
         },
+    },
+    define: {
+        'process.env': {},
     },
 });
