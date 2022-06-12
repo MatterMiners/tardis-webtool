eab5325cfa28b6e33ed0350917c16e8b425eaf20
Alexander Haas <mail@haas-alexander.com>
2022-06-12 14:21:59 +0200

    feat(project): :sparkles: Added the click-outside directive
    This directive enables to trigger an event only if the user clicked outside of the parent element

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,21 @@ import router from './router/router';
 
 const app = createApp(App);
 
+app.directive('click-outside', {
+  created(element, binding) {
+    element.clickOutsideEvent = (event: { target: any }) => {
+      //  check that click was outside the el and his children
+      if (!(element === event.target || element.contains(event.target))) {
+        binding.value(); // run callback function
+      }
+    };
+    document.body.addEventListener('click', element.clickOutsideEvent);
+  },
+  unmounted(element) {
+    document.body.removeEventListener('click', element.clickOutsideEvent);
+  },
+});
+
 app.use(router);
 app.use(pinia);
 app.mount('#app');
