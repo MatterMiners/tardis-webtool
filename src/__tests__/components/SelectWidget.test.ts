1a10dc2b3b0f2026a73e446d6677c50403314a35
Alexander Haas <mail@haas-alexander.com>
2022-06-14 20:22:51 +0200

    test(components): :white_check_mark: Added passing tests for SelectWidget

--- src/__tests__/components/SelectWidget.test.ts
+++ src/__tests__/components/SelectWidget.test.ts
@@ -1,14 +1,48 @@
 import { render, screen, fireEvent } from '@testing-library/vue';
 import SelectWidget from '@/components/util/SelectWidget.vue';
-import { appendFile } from 'fs';
 
-test('renders correctly', () => {
-  render(SelectWidget, {
-    props: {
-      data: ['foo', 'bar'],
-      label: 'XXX',
-    },
+function isCollapsed() {
+  expect(screen.queryByRole('list')).toBeFalsy();
+  expect(screen.queryByText('foo')).toBeNull();
+  expect(screen.queryByText('bar')).toBeNull();
+}
+
+describe('SelectWidget', () => {
+  beforeEach(() => {
+    render(SelectWidget, {
+      props: {
+        data: ['foo', 'bar'],
+        label: 'XXX',
+      },
+    });
   });
 
-  expect(screen.getByText(/XXX/)).toBeTruthy();
+  test('renders correctly', () => {
+    expect(screen.getByRole('button')).toBeTruthy();
+    expect(screen.getByText('XXX')).toBeTruthy();
+  });
+
+  test('is collapsed initially', () => {
+    isCollapsed();
+  });
+
+  test('is expanded when clicked', async () => {
+    await fireEvent.click(screen.getByRole('button'));
+    expect(screen.getByRole('list')).toBeTruthy();
+    expect(screen.getByText('foo')).toBeTruthy();
+    expect(screen.getByText('bar')).toBeTruthy();
+  });
+
+  test('is collapsed when clicked again', async () => {
+    await fireEvent.click(screen.getByRole('button'));
+    await fireEvent.click(screen.getByRole('button'));
+    isCollapsed();
+  });
+
+  // Not sure if this really clicks outside the button
+  test('is collapsed when clicked outside', async () => {
+    await fireEvent.click(screen.getByRole('button'));
+    await fireEvent.click(document.body);
+    isCollapsed();
+  });
 });
